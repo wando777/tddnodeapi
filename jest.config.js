853b44b37ec98223dcb2287e66173cc7b5500ebf
wando777 <wanderson.eq2011@gmail.com>
2022-11-09 01:53:45 -0300

    chore: add jest to lint staged

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
   roots: ['<rootDir>/src'],
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts>'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest '
+    '.+\\.ts$': 'ts-jest'
   }
 }
