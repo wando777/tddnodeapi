16f23b8091084ed02e71a6b5d2f79efe9bdb2b7b
wando777 <wanderson.eq2011@gmail.com>
2022-11-12 20:07:50 -0300

    feat: ensure signupcontroller returns 400 when no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/indent */
+export class SignUpController {
+    handle(httpRequest: any): any {
+        return {
+            statusCode: 400
+        }
+    }
+}
