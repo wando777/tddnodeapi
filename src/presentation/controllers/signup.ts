9505054bc06a6d5c2cdf26d56c93fe17a3de7b31
wando777 <wanderson.eq2011@gmail.com>
2022-11-15 18:01:20 -0300

    refactor: simplifying signupcode

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,11 +5,11 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse | undefined {
         try {
-            if (httpRequest.body.name === undefined) {
-                return badRequest(new MissingParamError('name'))
-            }
-            if (!httpRequest.body.email) {
-                return badRequest(new MissingParamError('email'))
+            const requiredFields = ['name', 'email']
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
             }
         } catch (err) {
             console.log(err)
