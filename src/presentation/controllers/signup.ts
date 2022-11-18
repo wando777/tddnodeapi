28cc0441bdfd25b7b330cf4a6e10a76e2e8b25ce
wando777 <wanderson.eq2011@gmail.com>
2022-11-18 00:17:10 -0300

    refactor: create server error as a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpResponse, HttpRequest } from '../protocols/http'
@@ -28,10 +27,7 @@ export class SignUpController implements Controller {
             }
         } catch (error) {
             console.log(error)
-            return {
-                statusCode: 500,
-                body: new ServerError()
-            }
+            return serverError()
         }
     }
 }
