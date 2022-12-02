816cff0249bc970ecb0cf1d974306c27ae0c1476
wando777 <wanderson.eq2011@gmail.com>
2022-12-01 23:13:22 -0300

    test: ensure BcrypterAdapter retruns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,14 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+const hashMocked = 'hashMocked'
+
+jest.mock('bcrypt', () => ({
+    async hash(): Promise<string> {
+        return await new Promise(resolve => resolve(hashMocked))
+    }
+}))
+
 describe('Bcrypt Adapter', () => {
     it('Should call bcrypt with correct values', async () => {
         const salt = 12
@@ -9,5 +17,12 @@ describe('Bcrypt Adapter', () => {
         const anyValue = 'any_value'
         await sut.encrypt(anyValue)
         expect(hashSpy).toHaveBeenCalledWith(anyValue, salt)
+    })
+    it('Should return a hash on succes', async () => {
+        const salt = 12
+        const sut = new BcryptAdapter(salt)
+        const anyValue = 'any_value'
+        const hash = await sut.encrypt(anyValue)
+        expect(hash).toBe(hashMocked)
     })
 })
