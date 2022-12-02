0efa5a46d3afa9cf1cf90e62030136667225969a
wando777 <wanderson.eq2011@gmail.com>
2022-12-01 23:03:53 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/method-signature-style */
 import { HttpRequest, HttpResponse } from './http'
 
 export interface Controller {
-    handle (httpRequest: HttpRequest): Promise<HttpResponse | undefined>
+    handle: (httpRequest: HttpRequest) => Promise<HttpResponse | undefined>
 }
