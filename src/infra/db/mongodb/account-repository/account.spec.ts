e45992d127be7b115df75930596c649172e4eca8
wando777 <wanderson.eq2011@gmail.com>
2022-12-04 13:53:47 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,32 @@
+import { AccountMongoRepository } from './account'
+import { MongoHelper } from './helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import env from '../../../../main/config/env'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let accountCollection: Collection
+
+describe('Account Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(env.mongoUrl)
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    });
+
+    it('Should return an account on success', async () => {
+        const sut = new AccountMongoRepository()
+        const accountTest = {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        }
+        const account = await sut.add(accountTest)
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe(accountTest.name)
+        expect(account.email).toBe(accountTest.email)
+        expect(account.password).toBe(accountTest.password)
+    })
+})
