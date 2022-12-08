68687a03f7b8a0756682de547d3a63d90528be2b
wando777 <wanderson.eq2011@gmail.com>
2022-12-07 22:54:13 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/account-repository/helpers/mongo-helper'
 
-app.listen(5050, () => console.log('Server running at http://local:5050'))
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+MongoHelper.connect(env.mongoUrl)
+    .then(async () => {
+        const app = (await import('./config/app')).default
+        app.listen(env.port, () => console.log(`Server running at http://local:${env.port}`))
+    }).catch(console.error)
