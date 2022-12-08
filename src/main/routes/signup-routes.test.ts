eb78e17ad264284f861a7b00592ee8f7a2467b82
wando777 <wanderson.eq2011@gmail.com>
2022-12-07 22:54:20 -0300

    test: connect to mongodb before app listen

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,24 @@
+import { Collection } from 'mongodb';
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/account-repository/helpers/mongo-helper';
 import app from '../config/app'
+import env from '../config/env';
+
+let accountCollection: Collection
 
 describe('SignUp Routes', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(env.mongoUrl)
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    });
+
+    beforeEach(async () => {
+        accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
     it('Should return an account on succes', async () => {
         await request(app)
             .post('/api/signup')
