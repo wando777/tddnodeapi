ca85a4ec39df22dba203791b172a385b7c2a9ed3
wando777 <wanderson.eq2011@gmail.com>
2022-12-11 13:05:19 -0300

    feat: ensure LogControllerDecorator call LogErrorrepository if controller returns a server error

--- src/data/protocols/sut-types.ts
+++ src/data/protocols/sut-types.ts
@@ -1,9 +1,18 @@
+import { LogControllerDecorator } from '../../main/decorators/log'
+import { Controller } from '../../presentation/protocols'
 import { DbAddAccount } from '../usecases/add-account/db-add-account'
 import { AddAccountRepository } from './add-account-repository'
 import { Encrypter } from './encrypter'
+import { LogErrorRepository } from './log-error-repository'
 
 export interface SutTypes {
     sut: DbAddAccount
     encrypterStub: Encrypter
     addAccountRepositoryStub: AddAccountRepository
+}
+
+export interface SutTypesDecorator {
+    sut: LogControllerDecorator
+    controllerStub: Controller
+    logErrorRepositoryStub: LogErrorRepository
 }
