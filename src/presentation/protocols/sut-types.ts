6c9a0b62e1a2c11f79d7fcc900cbb106f3b3a8bc
wando777 <wanderson.eq2011@gmail.com>
2022-12-19 07:46:40 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/protocols/sut-types.ts
+++ src/presentation/protocols/sut-types.ts
@@ -1,13 +1,13 @@
-import { AddAccount } from '../../domain/usecases/add-account'
 import { Authentication } from '../../domain/usecases/authentication'
 import { LoginController } from '../controllers/login/login'
 import { SignUpController } from '../controllers/singup/signup'
-import { EmailValidator } from './email-validator'
+import { AddAccount, EmailValidator, Validation } from '../controllers/singup/signup-protocols'
 
 export interface SutTypes {
     sut: SignUpController
     emailValidatorStub: EmailValidator
     addAccountStub: AddAccount
+    validationStub: Validation
 }
 
 export interface SutTypesLogin {
