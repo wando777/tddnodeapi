39ff68800190ddfebead8959862ce9d3e8525c14
wando777 <wanderson.eq2011@gmail.com>
2022-12-20 07:40:38 -0300

    refactor: move compare responsability from SignupController to ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldValidation } from '../../presentation/helpers/validators/compare-field-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/requeired-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
@@ -9,10 +10,11 @@ describe('SignUPValidation Factory', () => {
     it('Should ValidationComposite with all validations', () => {
         makeSignUpValidation()
         const validations: Validation[] = []
-        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-        for (const field of requiredFields) {
+        const fields = ['name', 'email', 'password', 'passwordConfirmation']
+        for (const field of fields) {
             validations.push(new RequiredFieldValidation(field))
         }
+        validations.push(new CompareFieldValidation(fields[3], fields[4]))
         expect(ValidationComposite).toHaveBeenCalledWith(validations)
     })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,12 +1,14 @@
+import { CompareFieldValidation } from '../../presentation/helpers/validators/compare-field-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/requeired-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 
 export const makeSignUpValidation = (): ValidationComposite => {
     const validations: Validation[] = []
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
+    const fields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of fields) {
         validations.push(new RequiredFieldValidation(field))
     }
+    validations.push(new CompareFieldValidation(fields[3], fields[4]))
     return new ValidationComposite(validations)
 }
