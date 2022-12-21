588b837715b6171b556ebf02c3a95edade91ed3f
wando777 <wanderson.eq2011@gmail.com>
2022-12-21 08:25:00 -0300

    test: ensure RequiredFieldValidation not return if validation works

--- src/presentation/helpers/validators/requeired-field-validation.ts
+++ src/presentation/helpers/validators/requeired-field-validation.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from '../../errors';
 import { Validation } from './validation';
 
 export class RequiredFieldValidation implements Validation {
-    private readonly fieldName: string;
+    private readonly fieldName: string
 
     constructor(fieldName: string) {
         this.fieldName = fieldName
--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -3,8 +3,15 @@ import { RequiredFieldValidation } from './requeired-field-validation'
 
 describe('RequiredField Validation', () => {
     it('Should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('any_field')
+        const fieldName = 'any_field'
+        const sut = new RequiredFieldValidation(fieldName)
         const response = sut.validate({ name: 'any_name' })
-        expect(response).toEqual(new MissingParamError('any_field'))
+        expect(response).toEqual(new MissingParamError(fieldName))
+    })
+    it('Should not return a MissingParamError if validation works', () => {
+        const fieldName = 'any_field'
+        const sut = new RequiredFieldValidation(fieldName)
+        const response = sut.validate({ any_field: 'any_name' })
+        expect(response).toBeFalsy()
     })
 })
