69c698e9e5ec96f86b8dbe55b1397c0296e910ab
wando777 <wanderson.eq2011@gmail.com>
2022-12-21 09:04:54 -0300

    test: add tests about CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,28 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldValidation } from './compare-field-validation'
+
+const makeFakeRequest = (): any => {
+    return {
+        fieldName: 'any_field',
+        fieldNameToCompare: 'any_field_to_compare'
+    }
+}
+
+const makeSut = (): CompareFieldValidation => {
+    // injecting a dependecy to our implementation where it should return whats has been mocked.
+    return new CompareFieldValidation(makeFakeRequest().fieldName, makeFakeRequest().fieldNameToCompare)
+}
+
+describe('CompareField Validation', () => {
+    it('Should return a InvalidParamError if validation fails', () => {
+        const sut = makeSut()
+        const fieldNameToCompare = makeFakeRequest().fieldNameToCompare
+        const response = sut.validate({ any_field: 'any_value', any_field_to_compare: 'wrong_value' })
+        expect(response).toEqual(new InvalidParamError(fieldNameToCompare))
+    })
+    it('Should not return an InvalidParamError if validation works', () => {
+        const sut = makeSut()
+        const response = sut.validate({ any_field: 'any_value', any_field_to_compare: 'any_value' })
+        expect(response).toBeFalsy()
+    })
+})
--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -1,16 +1,24 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './requeired-field-validation'
 
-describe('RequiredField Validation', () => {
+const makeFakeRequest = (): string => {
+    const fieldName = 'any_field'
+    return fieldName
+}
+
+const makeSut = (): RequiredFieldValidation => {
+    // injecting a dependecy to our implementation where it should return whats has been mocked.
+    return new RequiredFieldValidation(makeFakeRequest())
+}
+
+describe('RequiredField Validation', () => { 
     it('Should return a MissingParamError if validation fails', () => {
-        const fieldName = 'any_field'
-        const sut = new RequiredFieldValidation(fieldName)
+        const sut = makeSut()
         const response = sut.validate({ name: 'any_name' })
-        expect(response).toEqual(new MissingParamError(fieldName))
+        expect(response).toEqual(new MissingParamError(makeFakeRequest()))
     })
     it('Should not return a MissingParamError if validation works', () => {
-        const fieldName = 'any_field'
-        const sut = new RequiredFieldValidation(fieldName)
+        const sut = makeSut()
         const response = sut.validate({ any_field: 'any_name' })
         expect(response).toBeFalsy()
     })
