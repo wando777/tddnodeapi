cecd967b21eb5dd6d731f5ea9f4bdeb8b4d64efe
wando777 <wanderson.eq2011@gmail.com>
2023-01-03 09:23:59 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/domain/usecases/authentication/sut-types.ts
+++ src/domain/usecases/authentication/sut-types.ts
@@ -1,7 +1,9 @@
+import { HashComparer } from '../../../data/protocols/criptography/hash-comparer';
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository';
 import { DbAuthentication } from './db-authentication';
 
 export interface SutTypesDbAuthentication{
     sut: DbAuthentication
     loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+    hashComparerStub: HashComparer
 }
