2e90cd06c1553ba548956d843230515918de6085
wando777 <wanderson.eq2011@gmail.com>
2023-01-04 09:20:37 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/domain/usecases/authentication/db-authentication.ts
+++ src/domain/usecases/authentication/db-authentication.ts
@@ -1,18 +1,24 @@
 import { HashComparer } from '../../../data/protocols/criptography/hash-comparer';
+import { TokenGenerator } from '../../../data/protocols/criptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository';
 import { Authentication, AuthenticationModel } from '../authentication';
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
     private readonly hashComparer: HashComparer;
-    constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
+    private readonly tokenGenerator: TokenGenerator;
+    constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
         this.hashComparer = hashComparer
+        this.tokenGenerator = tokenGenerator
     }
 
     async auth(authentication: AuthenticationModel): Promise<string> {
         const account = await this.loadAccountByEmailRepository.load(authentication.email)
-        if (account) await this.hashComparer.compare(authentication.password, account.password)
+        if (account) {
+            await this.hashComparer.compare(authentication.password, account.password)
+            await this.tokenGenerator.generate( .id)
+        }
         return null
     }
 }
--- src/domain/usecases/authentication/sut-types.ts
+++ src/domain/usecases/authentication/sut-types.ts
@@ -1,4 +1,5 @@
 import { HashComparer } from '../../../data/protocols/criptography/hash-comparer';
+import { TokenGenerator } from '../../../data/protocols/criptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository';
 import { DbAuthentication } from './db-authentication';
 
@@ -6,4 +7,5 @@ export interface SutTypesDbAuthentication{
     sut: DbAuthentication
     loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
     hashComparerStub: HashComparer
+    tokenGeneratorStub: TokenGenerator
 }
