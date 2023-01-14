e484be9a0a9d4bf2e2d89a65becc8453d640d685
wando777 <wanderson.eq2011@gmail.com>
2023-01-14 17:18:24 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,8 @@
+import { hash } from 'bcrypt';
 import { Collection } from 'mongodb';
-import { describe } from 'node:test';
 import request from 'supertest'
+import { AddAccountModel } from '../../domain/usecases/add-account';
+import { SignUpAccountModel } from '../../domain/usecases/signup-account';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app'
 import env from '../config/env';
@@ -21,15 +23,40 @@ describe('Login Routes', () => {
         await accountCollection.deleteMany({})
     })
 
+    const makeFakeAccount = (): SignUpAccountModel => ({
+        name: 'wando',
+        email: 'wando@mail.com',
+        password: '123senha',
+        passwordConfirmation: '123senha'
+    })
+
+    const makeAddFakeAccount = (): AddAccountModel => ({
+        name: 'wando',
+        email: 'wando@mail.com',
+        password: '123senha'
+    })
+
     describe('POST /signup', () => {
         it('Should return 200 on signup success', async () => {
             await request(app)
                 .post('/api/signup')
+                .send(makeFakeAccount())
+                .expect(200)
+        })
+    })
+    describe('POST /login', () => {
+        it('Should return 200 on login success', async () => {
+            const password = await hash(makeAddFakeAccount().password, 12)
+            await accountCollection.insertOne({
+                name: makeAddFakeAccount().name,
+                email: makeAddFakeAccount().email,
+                password
+            })
+            await request(app)
+                .post('/api/login')
                 .send({
-                    name: 'wando',
                     email: 'wando@mail.com',
-                    password: '123senha',
-                    passwordConfirmation: '123senha'
+                    password: '123senha'
                 })
                 .expect(200)
         })
