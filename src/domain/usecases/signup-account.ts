e55d0db17beeb8e209404e3300b1cdace7b4ffbf
wando777 <wanderson.eq2011@gmail.com>
2023-01-14 17:18:18 -0300

    feat: add login route

---
+++ src/domain/usecases/signup-account.ts
@@ -0,0 +1,6 @@
+export interface SignUpAccountModel {
+    name: string
+    email: string
+    password: string
+    passwordConfirmation: string
+}
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup/signup-factory'
 import { adapterRoute } from '../adapter/express/express-route-adapter'
+import { makeLoginController } from '../factories/login/login-factory'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 
 export default (router: Router): void => {
     router.post('/signup', adapterRoute(makeSignUpController()))
+
+    router.post('/login', adapterRoute(makeLoginController()))
 }
