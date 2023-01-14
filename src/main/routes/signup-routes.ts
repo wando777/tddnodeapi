8dda07bb43f1074a667753aa40b0c2279f124aba
wando777 <wanderson.eq2011@gmail.com>
2023-01-14 16:12:40 -0300

    refactor: removing utils layer and move validator adapters to the main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
-import { adapterRoute } from '../adapter/express-route-adapter'
+import { adapterRoute } from '../adapter/express/express-route-adapter'
 
 export default (router: Router): void => {
     router.post('/signup', adapterRoute(makeSignUpController()))
