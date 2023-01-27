7a6b5b151f7cff7a18f441357ecbd560600e9676
wando777 <wanderson.eq2011@gmail.com>
2023-01-26 21:51:28 -0300

    feat: ensure DbAddAccount calls loadAccountByEmailRepository with correct email

--- src/data/protocols/sut-types.ts
+++ src/data/protocols/sut-types.ts
@@ -4,11 +4,13 @@ import { DbAddAccount } from '../usecases/add-account/db-add-account'
 import { AddAccountRepository } from './db/account/add-account-repository'
 import { Hasher } from './criptography/hasher'
 import { LogErrorRepository } from './db/log/log-error-repository'
+import { LoadAccountByEmailRepository } from './db/account/load-account-by-email-repository'
 
 export interface SutTypes {
     sut: DbAddAccount
     hasherStub: Hasher
     addAccountRepositoryStub: AddAccountRepository
+    loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
 }
 
 export interface SutTypesDecorator {
