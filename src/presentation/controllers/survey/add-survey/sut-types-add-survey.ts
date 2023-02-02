8f2688e97154a2535ae42f577397f5f43632373e
wando777 <wanderson.eq2011@gmail.com>
2023-02-02 10:01:38 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/sut-types-add-survey.ts
+++ src/presentation/controllers/survey/add-survey/sut-types-add-survey.ts
@@ -1,7 +1,8 @@
 import { AddSurveyController } from './add-survey-controller';
-import { Validation } from './add-survey-controller-protocols';
+import { AddSurvey, Validation } from './add-survey-controller-protocols';
 
 export interface SutTypes {
   sut: AddSurveyController
   validationStub: Validation
+  addSurveyStub: AddSurvey
 }
