8f671caa55d9ccb1ba191ce8d35ce068b17e7872
wando777 <wanderson.eq2011@gmail.com>
2023-02-14 21:53:03 -0300

    test: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-survey/db-load-survey.spec.ts
@@ -0,0 +1,61 @@
+import { DbLoadSurvey } from './db-load-survey'
+import MockDate from 'mockdate'
+import { LoadSurveyRepository } from './db-load-survey-protocols'
+import { SurveyModel } from '../../../domain/models/survey'
+
+const makeFakeSurveys = (): SurveyModel[] => [{
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+}]
+
+const makeLoadSurveyRepository = (): LoadSurveyRepository => {
+  class LoadSurveyRepositoryStub implements LoadSurveyRepository {
+    async loadAll(): Promise<SurveyModel[]> {
+      return await new Promise(resolve => resolve(makeFakeSurveys()))
+    }
+  }
+  return new LoadSurveyRepositoryStub()
+}
+
+export interface SutTypes {
+  sut: DbLoadSurvey
+  loadSurveyRepositoryStub: LoadSurveyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyRepositoryStub = makeLoadSurveyRepository()
+  const sut = new DbLoadSurvey(loadSurveyRepositoryStub)
+  return {
+    sut,
+    loadSurveyRepositoryStub
+  }
+}
+
+describe('DbAddSurvey Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
+  it('Should call LoadSurveyRepository', async () => {
+    const { sut, loadSurveyRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveyRepositoryStub, 'loadAll')
+    await sut.load()
+    expect(loadSpy).toHaveBeenCalled()
+  })
+  // it('Should throw an error if AddSurveyRepository throws', async () => {
+  //   const { sut, addSurveyRepositoryStub } = makeSut();
+  //   jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+  //   // jest.spyOn(addSurveyRepositoryStub, 'add').mockImplementationOnce(async () => {
+  //   //     throw new Error()
+  //   // })
+  //   const accountPromise = sut.add(makeFakeSurvey())
+  //   await expect(accountPromise).rejects.toThrow()
+  // })
+})
