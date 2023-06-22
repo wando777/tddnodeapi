7b163a7e0ad512bdb57a0afb84b452d8ff7d9372
wando777 <wanderson.eq2011@gmail.com>
2023-06-22 08:54:02 -0300

    fix: change aggregation query to return all the answers correctly

--- src/infra/db/mongodb/save-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/save-result/survey-result-mongo-repository.spec.ts
@@ -67,6 +67,8 @@ describe('Survey Result Mongo Repository', () => {
             expect(surveyResult).toBeTruthy()
             expect(surveyResult.answers[0].count).toBe(1)
             expect(surveyResult.answers[0].percent).toBe(100)
+            expect(surveyResult.answers[1].count).toBe(0)
+            expect(surveyResult.answers[1].percent).toBe(0)
         })
         // it('Should update survey on success', async () => {
         //     const sut = makeSut()
@@ -109,6 +111,8 @@ describe('Survey Result Mongo Repository', () => {
             expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
             expect(surveyResult.answers[0].count).toBe(1)
             expect(surveyResult.answers[0].percent).toBe(100)
+            expect(surveyResult.answers[1].count).toBe(0)
+            expect(surveyResult.answers[1].percent).toBe(0)
           })
     })
 })
