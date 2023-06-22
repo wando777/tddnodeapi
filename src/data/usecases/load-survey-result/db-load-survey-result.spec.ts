19e2a5ef65ad9b8892bb9a9c93674c9eede2b0fe
wando777 <wanderson.eq2011@gmail.com>
2023-06-22 09:39:24 -0300

    test: ensure DbLoadSurveyResult calls LoadSurveyResultRespository with correct values

---
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,26 @@
+import { mockLoadSurveyResultRepository } from '@/data/test/mock-db-load-survey-result'
+import { DbLoadSurveyResult } from './db-load-survey-result'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  return {
+    sut,
+    loadSurveyResultRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyResult Usecase', () => {
+  it('Should call LoadSurveyResultRepository ', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_survey_id')
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})
