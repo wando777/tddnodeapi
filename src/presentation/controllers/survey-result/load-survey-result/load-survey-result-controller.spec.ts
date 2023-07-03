79bd87bf89c80221c443e24c89e60b331da44d46
wando777 <wanderson.eq2011@gmail.com>
2023-07-03 20:16:35 -0300

    test: ensure controller Calls LoadSurveyById with correct values & return 404 when there's no survey

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,47 @@
+import { mockLoadSurveyById } from '@/presentation/test'
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
+import { notFound } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
+import MockDate from 'mockdate'
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+type SutTypes = {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
+describe('SaveSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
+  it('Should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(mockRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith(mockRequest().params.surveyId)
+  })
+  it('Should returns 404 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const httpresponse = await sut.handle(mockRequest())
+    expect(httpresponse).toEqual(notFound(new InvalidParamError('Survey not found')))
+  })
+})
