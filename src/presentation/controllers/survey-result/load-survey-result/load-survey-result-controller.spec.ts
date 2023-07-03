dc8c29fb3f3aaeeb2d477cdfea015681d7c6385e
wando777 <wanderson.eq2011@gmail.com>
2023-07-03 20:22:44 -0300

    test: ensure controller return 403 instead of 404 when an invalid surveyId is provided

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,7 +1,7 @@
 import { mockLoadSurveyById } from '@/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
 import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
-import { notFound } from '@/presentation/helpers/http/http-helper'
+import { forbidden, notFound } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 import MockDate from 'mockdate'
 
@@ -38,10 +38,16 @@ describe('SaveSurveyResult Controller', () => {
     await sut.handle(mockRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith(mockRequest().params.surveyId)
   })
-  it('Should returns 404 if LoadSurveyById returns null', async () => {
+  // it('Should returns 404 if LoadSurveyById returns null', async () => {
+  //   const { sut, loadSurveyByIdStub } = makeSut()
+  //   jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+  //   const httpresponse = await sut.handle(mockRequest())
+  //   expect(httpresponse).toEqual(notFound(new InvalidParamError('Survey not found')))
+  // })
+  it('Should returns 403 if LoadSurveyById returns null', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
     const httpresponse = await sut.handle(mockRequest())
-    expect(httpresponse).toEqual(notFound(new InvalidParamError('Survey not found')))
+    expect(httpresponse).toEqual(forbidden(new InvalidParamError('Survey not found')))
   })
 })
