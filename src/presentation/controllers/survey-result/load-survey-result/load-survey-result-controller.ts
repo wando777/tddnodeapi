de4359b89c25c06286d239943a800263a4f863b3
wando777 <wanderson.eq2011@gmail.com>
2023-07-03 20:56:47 -0300

    feat: ensure LoadSurveyResultController calls loadSurveyResult with correct values

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,17 +1,24 @@
-import { forbidden } from '@/presentation/helpers/http/http-helper';
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols';
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper';
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, LoadSurveyResult } from './load-survey-result-controller-protocols';
 import { InvalidParamError } from '@/presentation/errors';
 
 export class LoadSurveyResultController implements Controller {
   constructor(
-    private readonly loadSurveyById: LoadSurveyById
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
   ) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    if (!survey) {
-      return forbidden(new InvalidParamError('Survey not found'))
+    try {
+      const { surveyId } = httpRequest.params
+      const survey = await this.loadSurveyById.loadById(surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('Survey not found'))
+      }
+      await this.loadSurveyResult.load(surveyId)
+      return ok(survey)
+    } catch (error: any) {
+      return serverError(error)
     }
-    return await Promise.resolve(null)
   }
 }
