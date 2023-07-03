ff6d0408a05b39145ec785b9cfb2da0e17571a67
wando777 <wanderson.eq2011@gmail.com>
2023-07-03 20:22:37 -0300

    fix: ensure controller return 403 instead of 404 when an invalid surveyId is provided

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,4 +1,4 @@
-import { notFound } from '@/presentation/helpers/http/http-helper';
+import { forbidden } from '@/presentation/helpers/http/http-helper';
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols';
 import { InvalidParamError } from '@/presentation/errors';
 
@@ -10,7 +10,7 @@ export class LoadSurveyResultController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
     if (!survey) {
-      return notFound(new InvalidParamError('Survey not found'))
+      return forbidden(new InvalidParamError('Survey not found'))
     }
     return await Promise.resolve(null)
   }
