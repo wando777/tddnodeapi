b985ed657da52b3edb9f41f4d8183e2fb410c89b
wando777 <wanderson.eq2011@gmail.com>
2023-07-04 22:19:56 -0300

    test: ensure load survey route returns 403 when no accessToken is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -75,31 +75,31 @@ describe('Survey Result Routes', () => {
         //         .expect(403)
         // })
     })
-    // describe('GET /survey', () => {
-    //     it('Should return 403 on load survey without accessToken', async () => {
-    //         await request(app)
-    //             .get('/api/survey')
-    //             .expect(403)
-    //     })
-    //     it('Should return 204 on load survey with valid accessToken but there is no survey', async () => {
-    //         const accessToken = await makeAccessToken()
-    //         await request(app)
-    //             .get('/api/survey')
-    //             .set('x-access-token', accessToken)
-    //             .expect(204)
-    //     })
-    //     it('Should return 200 on load survey with valid accessToken', async () => {
-    //         await surveyCollection.insertOne(makeFakeSurvey())
-    //         await request(app)
-    //             .get('/api/survey')
-    //             .set('x-access-token', await makeAccessToken())
-    //             .expect(200)
-    //     })
-    //     // it('Should return 403 on add survey with an invalid accessToken', async () => {
-    //     //     await request(app)
-    //     //         .post('/api/survey')
-    //     //         .send(makeFakeSurvey())
-    //     //         .expect(403)
-    //     // })
-    // })
+    describe('GET /survey/:surveyId/result', () => {
+        it('Should return 403 on load survey without accessToken', async () => {
+            await request(app)
+                .get('/api/survey/any_id/result')
+                .expect(403)
+        })
+        // it('Should return 204 on load survey with valid accessToken but there is no survey', async () => {
+        //     const accessToken = await makeAccessToken()
+        //     await request(app)
+        //         .get('/api/survey')
+        //         .set('x-access-token', accessToken)
+        //         .expect(204)
+        // })
+        // it('Should return 200 on load survey with valid accessToken', async () => {
+        //     await surveyCollection.insertOne(makeFakeSurvey())
+        //     await request(app)
+        //         .get('/api/survey')
+        //         .set('x-access-token', await makeAccessToken())
+        //         .expect(200)
+        // })
+        // // it('Should return 403 on add survey with an invalid accessToken', async () => {
+        // //     await request(app)
+        // //         .post('/api/survey')
+        // //         .send(makeFakeSurvey())
+        // //         .expect(403)
+        // // })
+    })
 })
